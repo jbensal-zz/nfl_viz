ff9263d79ad9577ff18f6fb155004bb9b96e26d1
Raj Ramamurthy <raj@mynameisraj.com>
2014-02-01 13:50:12 -0600

    Cleanup and change text of button I was using double quotes on accident. Switch from onclick to an event listener so that all js is in the viz.js file.

--- js/viz.js
+++ js/viz.js
@@ -9,7 +9,7 @@ var players = new Array(); // array of all the players
 
 /* Load in the player data */
 var loadPlayerData = function() {
-  d3.csv("data/nfl_player_data.csv", function (player) {
+  d3.csv('data/nfl_player_data.csv', function (player) {
     /* Needed to filter out players born before 1900 because don't have
      * life expectancy data for them. */
     if (player.birth_year < 1900) return;
@@ -25,7 +25,7 @@ var loadPlayerData = function() {
   }, function(error, rows) {
     /* Player data is loaded */
     if (error) {
-      console.log("Error loading player data: ", error);
+      console.log('Error loading player data: ', error);
     }
     loadLifeExpectancy();
   });
@@ -34,12 +34,12 @@ var loadPlayerData = function() {
 /* Load in life expectancy data */
 var loadLifeExpectancy = function() {
   var lives = {};
-  d3.csv("data/life_expectancy.csv", function(data) {
+  d3.csv('data/life_expectancy.csv', function(data) {
     lives[data.year] = parseFloat(data.male_life_expectancy);
   }, function(error, rows) {
     /* Life expectancy data is loaded */
     if (error) {
-      console.log("Error loading life expectancy data:", error);
+      console.log('Error loading life expectancy data:', error);
     }
     /* Match life expectancy to player */
     for (var i = 0; i < players.length; i++) {
@@ -51,7 +51,7 @@ var loadLifeExpectancy = function() {
 
 /* The data is loaded at this point. */
 var dataIsLoaded = function() {
-  console.log("All data loaded.");
+  console.log('All data loaded.');
   calculate();
   makeDOMRepresentation();
 };
@@ -70,21 +70,37 @@ var calculate = function(){
 /* Draw the table to the DOM. */
 var makeDOMRepresentation = function() {
   /* Using a string is the fastest way, especially with lots of data */
-  var DOMRepresentation = "<table class='table table-striped'><thead><th>Name</th><th>Born</th><th>Died</th><th>Expectancy</th><th> Age at Death</th></thead><tbody>";
+  var DOMRepresentation = '<table class="table table-striped"><thead><th>Name</th><th>Born</th><th>Died</th><th>Expectancy</th><th> Age at Death</th></thead><tbody>';
   for (var i = 0; i < players.length; i++) {
-    DOMRepresentation += "<tr><td>" + players[i].name + "</td><td>" + players[i].birthYear + "</td><td>" + players[i].deathYear + "</td><td>" + players[i].lifeExpectancy + "</td><td>" + players[i].deathAge + "</td></tr>";
+    DOMRepresentation += '<tr><td>' + players[i].name + '</td><td>' + players[i].birthYear + '</td><td>' + players[i].deathYear + '</td><td>' + players[i].lifeExpectancy + '</td><td>' + players[i].deathAge + '</td></tr>';
   }
-  DOMRepresentation += "</tbody></table>";
-  var tableWrapper = document.createElement("div");
-  tableWrapper.id = "js-table-data";
+  DOMRepresentation += '</tbody></table>';
+  var tableWrapper = document.createElement('div');
+  tableWrapper.id = 'js-table-data';
   tableWrapper.innerHTML = DOMRepresentation;
-  document.getElementById("wrap").appendChild(tableWrapper);
-
+  document.getElementById('wrap').appendChild(tableWrapper);
   /* Hide loading message */
-  document.getElementById("message").style.display = "none";
+  document.getElementById('message').style.display = 'none';
+  /* Show toggle */
+  document.getElementById('toggle').style.display = 'block';
 };
 
-/* Add listener to load data on content ready */
-window.addEventListener("DOMContentLoaded", function() {
+/* Called when DOM is ready */
+var init = function() {
+  document.getElementById('toggle').addEventListener('click', function(e) {
+    e.preventDefault();
+    var div = document.getElementById('js-table-data');
+    if (div.style.display !== 'none') {
+      div.style.display = 'none';
+      this.innerHTML = 'Show';
+    }
+    else {
+      div.style.display = 'block';
+      this.innerHTML = 'Hide';
+    }
+  }, false);
   loadPlayerData();
-}, false);
+}
+
+/* Add listener to load data on content ready */
+window.addEventListener("DOMContentLoaded", init, false);
